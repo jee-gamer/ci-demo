61927ebd4771655fca60ee4d63a355b91cfd37d6
Jaykayy <jiramate2548@gmaill.com>
2024-09-16 15:28:43 +0700

    test cover 100%

--- test_statistics.py
+++ test_statistics.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from statistics import variance, stdev
+from statistics import variance, stdev, average
 from math import sqrt
 
 class StatisticsTest(TestCase):
@@ -17,6 +17,9 @@ class StatisticsTest(TestCase):
         # variance([0,4,4,8]) == 8
         self.assertAlmostEqual(8.0, variance([0.1, 4.1, 4.1, 8.1]))
 
+    def test_variance_empty(self):
+        self.assertRaises(ValueError, variance, [])
+
     def test_stdev(self):
         # standard deviation of a single value should be zero
         self.assertEqual(0.0, stdev([10.0]))
@@ -24,6 +27,12 @@ class StatisticsTest(TestCase):
         self.assertEqual(2.0, stdev([1, 5]))
         # variance([0, 0.5, 1, 1.5, 2.0]) is 0.5
         self.assertEqual(sqrt(0.5), stdev([0, 0.5, 1, 1.5, 2]))
+
+    def test_average(self):
+
+        self.assertRaises(ValueError, average, [])
+        self.assertEqual(average([1]), 1)
+        self.assertEqual(average([1, 2]), 1.5)
 
 
 if __name__ == '__main__':
